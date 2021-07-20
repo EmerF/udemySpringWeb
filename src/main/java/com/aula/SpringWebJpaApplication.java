3ce81d402513793671a16d4c3cf6f32395b7d80d
Emerson <griloemer@gmail.com>
2021-07-20 17:19:11 -0300

    Registro da servlet do faces context

--- src/main/java/com/aula/SpringWebJpaApplication.java
+++ src/main/java/com/aula/SpringWebJpaApplication.java
@@ -16,18 +16,17 @@ public class SpringWebJpaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebJpaApplication.class, args);
 	}
-	
-	@Bean
-	  ServletRegistrationBean<FacesServlet> jsfServletRegistration (ServletContext servletContext) {
-	      //spring boot only works if this is set 
-	      servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
 
-	      //registration
-	      ServletRegistrationBean<FacesServlet> srb = new ServletRegistrationBean<FacesServlet>();
-	      srb.setServlet(new FacesServlet());
-	      srb.setUrlMappings(Arrays.asList("*.xhtml"));
-	      srb.setLoadOnStartup(1);
-	      return srb;
-	  }
-	
+	// configs para execução do JSF
+	@Bean
+	ServletRegistrationBean<FacesServlet> jsfServletRegistration(ServletContext servletContext) {
+		servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+
+		ServletRegistrationBean<FacesServlet> srb = new ServletRegistrationBean<FacesServlet>();
+		srb.setServlet(new FacesServlet());
+		srb.setUrlMappings(Arrays.asList("*.xhtml"));
+		srb.setLoadOnStartup(1);
+		return srb;
+	}
+
 }
