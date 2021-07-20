6bb9607c54a1156a812ab698660f4a3cfdd1315c
Emerson <griloemer@gmail.com>
2021-07-20 16:46:05 -0300

    Pagina login jsf funcionando

--- src/main/java/com/aula/SpringWebJpaApplication.java
+++ src/main/java/com/aula/SpringWebJpaApplication.java
@@ -1,7 +1,14 @@
 package com.aula;
 
+import java.util.Arrays;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringWebJpaApplication {
@@ -9,5 +16,18 @@ public class SpringWebJpaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebJpaApplication.class, args);
 	}
+	
+	@Bean
+	  ServletRegistrationBean<FacesServlet> jsfServletRegistration (ServletContext servletContext) {
+	      //spring boot only works if this is set 
+	      servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
 
+	      //registration
+	      ServletRegistrationBean<FacesServlet> srb = new ServletRegistrationBean<FacesServlet>();
+	      srb.setServlet(new FacesServlet());
+	      srb.setUrlMappings(Arrays.asList("*.xhtml"));
+	      srb.setLoadOnStartup(1);
+	      return srb;
+	  }
+	
 }
