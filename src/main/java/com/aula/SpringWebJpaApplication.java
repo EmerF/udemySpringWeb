ea539b6bfdae6bae46707b6c0dcda0894a256fec
Emerson <griloemer@gmail.com>
2022-08-13 12:40:32 -0300

    Rodando projeto com joinfaces

--- src/main/java/com/aula/SpringWebJpaApplication.java
+++ src/main/java/com/aula/SpringWebJpaApplication.java
@@ -1,13 +1,7 @@
 package com.aula;
 
-import javax.faces.webapp.FacesServlet;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-
-import com.aula.model.Login;
 
 @SpringBootApplication
 public class SpringWebJpaApplication {
@@ -15,13 +9,4 @@ public class SpringWebJpaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebJpaApplication.class, args);
 	}
-	
-	@Bean
-	public ServletRegistrationBean<FacesServlet> servletRegistration(){
-		return new ServletRegistrationBean<FacesServlet>(new FacesServlet(), "*.jsf");
-	}
-		
-	/*
-	 * @Bean public Login loginMapper() { return new Login (); }
-	 */
 }
--- src/test/java/com/aula/SpringWebJpaApplicationTests.java
+++ src/test/java/com/aula/SpringWebJpaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringWebJpaApplicationTests {
 
-	@Test
+	/*@Test
 	void contextLoads() {
-	}
+	}*/
 
 }
